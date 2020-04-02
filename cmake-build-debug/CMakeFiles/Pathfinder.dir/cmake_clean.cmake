8cbd2f75440e89c236aa3759b1c760790eaa7450
Albert Vu <44858018+minhlongvu@users.noreply.github.com>
2020-04-02 14:49:09 -0500

    -Changed Project Name -Added A* Search with Heuristic function for neighbors -Added CellSort function -Added ExpandNeighbors function -Added CheckValidCell function for neighbors -Added AddToOpen function for neighbors

---
+++ cmake-build-debug/CMakeFiles/Pathfinder.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pathfinder.dir/main.cpp.obj"
+  "Pathfinder.exe"
+  "Pathfinder.exe.manifest"
+  "Pathfinder.lib"
+  "Pathfinder.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pathfinder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
